b85ed0766df414f1c7cee2d7b716f872b64bd3e7
wangjunpeng <18438622750@163.com>
2022-10-20 11:37:36 +0800

    🎉 init: 初始化

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Index from '../views/Index.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: Index,
+    children: [
+
+    ]
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
