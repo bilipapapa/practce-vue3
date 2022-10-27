af668221c0597ceef50951546ac83d3c344dc0d5
wangjunpeng <18438622750@163.com>
2022-10-27 09:51:33 +0800

    🌈 style: 代码格式规则调整

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import './registerServiceWorker';
+import './registerServiceWorker'
 
-
-
-
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app');
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,18 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Index from '../views/Index.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Index from '../views/Index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: Index,
-    children: [
-
-    ]
+    children: [],
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
